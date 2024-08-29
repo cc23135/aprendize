9e24acbe8cce6d24c4cac965a40c519bcb3fd79d
ARTHUR GAMA JORGE <arthurgamajorgetec@gmail.com>
2024-08-29 08:45:49 -0300

    Completar a conexão com banco de dados através de node prisma

--- ApiNode/Api/index.js
+++ ApiNode/Api/index.js
@@ -1,17 +1,26 @@
-require('dotenv').config();
-
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
 const app = express();
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Rota de exemplo
-app.get('/api/hello', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello, World!');
 });
 
-// Inicia o servidor
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.usuario.findMany(); // Ajuste o nome do modelo para o seu caso
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`API rodando na porta ${port}`);
+  console.log('Servidor rodando na porta ' + port);
 });
