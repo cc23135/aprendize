90ab82598dc51cfeb9f8ae776209a5db46384c8e
ARTHUR GAMA JORGE <arthurgamajorgetec@gmail.com>
2024-09-02 09:33:19 -0300

    Implementar sistema de upload de imagem para google cloud storage

--- ApiNode/Api/index.js
+++ ApiNode/Api/index.js
@@ -1,10 +1,28 @@
-require('dotenv').config({ path: '../.env' }); // Ajuste o caminho se necessário
+require('dotenv').config({ path: '../.env' }); 
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const cors = require('cors');
+const multer = require('multer');
+const { Storage } = require('@google-cloud/storage');
+const { v4: uuidv4 } = require('uuid');
+const sharp = require('sharp');
+const path = require('path');
 
-const app = express(); // Inicialização do Express
+const app = express(); 
 const prisma = new PrismaClient();
+
+const storage = new Storage({
+  keyFilename: path.join(__dirname, '..', 'codedrafts-401521-9a4d49f88703.json'), 
+  projectId: 'codedrafts-401521',
+});
+const bucketName = 'imagesaprendize'; 
+
+const upload = multer({
+  storage: multer.memoryStorage(), // Armazena o arquivo na memória temporariamente
+  limits: {
+    fileSize: 5 * 1024 * 1024, // Limite de 5MB para o tamanho do arquivo
+  },
+});
 
 const port = process.env.PORT || 3000;
 
@@ -21,9 +39,55 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.post('/api/upload-image', upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      console.error('Nenhum arquivo enviado');
+      return res.status(400).json({ error: 'Nenhum arquivo enviado' });
+    }
+    console.log("Arquivo recebido:", req.file.originalname);
+
+    // Otimiza a imagem com sharp
+    const optimizedImageBuffer = await sharp(req.file.buffer)
+      .resize(800)
+      .jpeg({ quality: 80 })
+      .toBuffer();
+
+    console.log("Imagem otimizada");
+
+    const uniqueFileName = `${uuidv4()}-${req.file.originalname}`;
+    console.log('Nome único do arquivo:', uniqueFileName);
+    console.log('Project ID:', storage.projectId);
+
+    const bucket = storage.bucket(bucketName);
+    const blob = bucket.file(uniqueFileName);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+    });
+
+    blobStream.on('error', (err) => {
+      console.error('Erro ao criar o stream:', err);
+      res.status(500).json({ error: err.message });
+    });
+
+    blobStream.on('finish', () => {
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+      console.log('URL pública do arquivo:', publicUrl);
+      res.status(200).json({ message: 'Upload bem-sucedido', url: publicUrl });
+    });
+
+    blobStream.end(optimizedImageBuffer);
+  } catch (error) {
+    console.error('Erro ao fazer upload da imagem:', error);
+    res.status(500).json({ error: 'Erro ao fazer upload da imagem' });
+  }
+});
+
+
+
 app.get('/api/users', async (req, res) => {
   try {
-    const users = await prisma.usuario.findMany(); // Ajuste o nome do modelo para o seu caso
+    const users = await prisma.usuario.findMany(); 
     res.json(users);
   } catch (error) {
     res.status(500).json({ error: 'Erro ao buscar usuários' });
--- flutter/lib/user_page.dart
+++ flutter/lib/user_page.dart
@@ -1,7 +1,81 @@
+import 'dart:typed_data';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart'; // Para verificar a plataforma
+import 'package:flutter/foundation.dart' show kIsWeb; // Verificar se é web
 import 'AppStateSingleton.dart';
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  final Dio _dio = Dio(); // Instância do Dio
+
+  Future<void> _pickImage() async {
+    final XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      if (kIsWeb) {
+        // Web
+        final bytes = await image.readAsBytes(); // Lê os bytes da imagem
+        final formData = FormData.fromMap({
+          'image': MultipartFile.fromBytes(bytes, filename: image.name),
+        });
+        await _uploadImageWeb(formData);
+      } else {
+        // Mobile
+        final imageFile = image.readAsBytes(); // Para mobile
+        final formData = FormData.fromMap({
+          'image': MultipartFile.fromBytes(await imageFile, filename: image.name),
+        });
+        await _uploadImageMobile(formData);
+      }
+    }
+  }
+
+  Future<void> _uploadImageWeb(FormData formData) async {
+    try {
+      final response = await _dio.post(
+        'http://localhost:6060/api/upload-image',
+        data: formData,
+      );
+
+      if (response.statusCode == 200) {
+        print('Upload bem-sucedido: ${response.data}');
+        setState(() {
+          AppStateSingleton().userProfileImageUrl = response.data['url']; // Atualize a URL conforme necessário
+        });
+      } else {
+        print('Falha ao enviar a imagem. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Erro ao enviar a imagem: $e');
+    }
+  }
+
+  Future<void> _uploadImageMobile(FormData formData) async {
+    try {
+      final response = await _dio.post(
+        'http://localhost:6060/api/upload-image',
+        data: formData,
+      );
+
+      if (response.statusCode == 200) {
+        print('Upload bem-sucedido: ${response.data}');
+        setState(() {
+          AppStateSingleton().userProfileImageUrl = response.data['url']; // Atualize a URL conforme necessário
+        });
+      } else {
+        print('Falha ao enviar a imagem. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Erro ao enviar a imagem: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,38 +90,40 @@ class UserPage extends StatelessWidget {
               alignment: Alignment.topRight,
               children: [
                 Container(
-                  width: 100, // Dobro do radius do CircleAvatar para cobrir o espaço total
+                  width: 100,
                   height: 100,
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     border: Border.all(
-                      color: Colors.white, // Cor da borda
-                      width: 4.0, // Largura da borda
+                      color: Colors.white,
+                      width: 4.0,
                     ),
                   ),
                   child: CircleAvatar(
                     radius: 50,
                     backgroundImage: AppStateSingleton().userProfileImageUrl.isNotEmpty
                         ? NetworkImage(AppStateSingleton().userProfileImageUrl)
-                        : AssetImage('assets/images/mona.png') as ImageProvider, // Imagem padrão se a URL estiver vazia
+                        : AssetImage('assets/images/mona.png') as ImageProvider,
                     backgroundColor: Colors.grey.shade800,
                   ),
                 ),
                 Positioned(
                   right: 0,
-                  child: CircleAvatar(
-                    radius: 15,
-                    backgroundColor: Colors.white,
-                    child: Icon(
-                      Icons.edit,
-                      size: 25,
-                      color: Colors.purple,
+                  child: GestureDetector(
+                    onTap: _pickImage,
+                    child: CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.edit,
+                        size: 25,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
-
             SizedBox(height: 20),
             // Nome do usuário
             Text(
@@ -124,7 +200,7 @@ class UserPage extends StatelessWidget {
             // Botão de logout
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.purple, // Cor de fundo
+                backgroundColor: Colors.purple,
                 padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30),
